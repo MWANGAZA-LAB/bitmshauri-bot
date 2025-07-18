250c99d824a656954f69d77c4ad0847603b39a37
MWANGAZA-LAB <mwanga02717@gmail.com>
2025-07-18 10:38:38 +0300

    Fixed deployment issues on railway

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,13 @@
 #from flask import Flask
-from config import Config
-import os
+#from config import Config
+#import os
 
 #app = Flask(__name__)
 #app.config.from_object(Config)
 
-print("✅ BitMshauri app initialized!")
+#print("✅ BitMshauri app initialized!")
 
 # Import bot components after app initialization
-from app.bot import telegram_bot
+#from app.bot import telegram_bot
 
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+#TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
