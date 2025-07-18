a46e50349ea959f162e0fee96e4ed75dca4b07aa
MWANGAZA-LAB <mwanga02717@gmail.com>
2025-07-18 11:26:14 +0300

    Fixed deployment errors

--- app/bot/price_api.py
+++ app/bot/price_api.py
@@ -1,5 +1,5 @@
 import requests
-from app import app
+#from app import app
 
 def get_bitcoin_price():
     try:
--- app/bot/telegram_bot.py
+++ app/bot/telegram_bot.py
@@ -1,3 +1,4 @@
+from dotenv import load_dotenv; load_dotenv()
 import logging
 from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import (
@@ -13,6 +14,7 @@ import random
 import os
 import requests
 from datetime import datetime, timedelta
+
 
 # Configure logging
 logging.basicConfig(
